c89ad2ea7c165393b2d8441861abdae917b38b8e
Bilderberg <bilderberg@MacBook-Pro-Bilderberg.local>
2026-04-02 12:07:38 +0300

    vol1

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import BurgerMenu from './components/burger-menu/BurgerMenu';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>
+          <BurgerMenu />
+          Hello world
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/declarations.d.ts
@@ -0,0 +1,24 @@
+declare module '*.css' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.sass' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.less' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
